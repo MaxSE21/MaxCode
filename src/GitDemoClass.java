48085492717ffc79189666eec75fdb6444583821
IsakBorovac <isak.borovac@gmail.com>
2021-11-03 16:47:36 +0100

    Mina fina ändringar på MAx kod

--- src/GitDemoClass.java
+++ src/GitDemoClass.java
@@ -1,8 +1,10 @@
 public class GitDemoClass {
     public static void main(String args []){
 
-        System.out.println("Max Code");
-        System.out.println("Max 2");
+        System.out.println("Max Code, wow MAx I changed your code");
+        System.out.println("Max 33");
+        System.out.println("Buttericks stänger 19");
+
 
 
 
